1acaa172435f509b646828fb362d319ccb6b89c0
saksh <sakshi.chaudhari019@gmail.com>
2024-08-18 22:14:12 +0530

    [sakshi]  adding features COV_TC_456_bannervalidation

---
+++ src/test/java/coverFoxTest/COV_TC_456_bannervalidation.java
@@ -0,0 +1,11 @@
+package coverFoxTest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class COV_TC_456_bannervalidation {
+  @Test
+  public void bannervalidation() {
+	  Reporter.log("Banner validation succesfull",true);
+  }
+}
